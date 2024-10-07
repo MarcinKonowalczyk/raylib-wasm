a11644c78c0ab19d8d3a4bead673dabd0056e661
Mark Tyrkba <awfulmarco@gmail.com>
2024-10-07 19:19:46 +0300

    Add some web things

--- raylib.rs
+++ raylib.rs
@@ -12,6 +12,14 @@ mod structs;
 #[allow(unused)]
 pub use structs::*;
 #[allow(unused, dead_code, non_camel_case_types, non_snake_case)]
+#[cfg(not(feature = "web"))]
 mod fns;
 #[allow(unused)]
+#[cfg(not(feature = "web"))]
 pub use fns::*;
+#[cfg(feature = "web")]
+#[allow(non_snake_case)]
+mod web_fns;
+#[allow(unused)]
+#[cfg(feature = "web")]
+pub use web_fns::*;
---
+++ web_fns.rs
@@ -0,0 +1,35 @@
+use crate::{Rectangle, KeyboardKey, Color};
+
+extern {
+    pub fn DrawRectangleRec_(_: *const Rectangle, _: *const Color);
+    pub fn DrawRectangle(_: i32, _: i32, _: i32, _: i32, _: Color);
+    pub fn SetTargetFPS(_: i32);
+    pub fn InitWindow(_: i32, _: i32, _: *const i8);
+    pub fn BeginDrawing();
+    pub fn ClearBackground_(_: *const Color);
+    pub fn GetFrameTime() -> f32;
+    pub fn DrawFPS(_: i32, _: i32);
+    pub fn DrawText_(_: *const i8, _: i32, _: i32, _: i32, _: *const Color);
+    pub fn EndDrawing();
+    pub fn SetExitKey(_: KeyboardKey);
+    pub fn CloseWindow();
+    pub fn IsKeyDown(_: KeyboardKey) -> bool;
+    pub fn GetFPS() -> i32;
+}
+
+// This functions are mandotory because without them all this won't work.
+//   You can't just pass a structure into the function and unwrap it' fields in JS via memory buffer,
+//   To the "unwrapping data via memory buffer" system to work we've got create this layer of
+//   abscration, maybe i'll find a way to avoid that, but for now idk
+
+pub unsafe fn ClearBackground(color: Color) {
+    ClearBackground_(std::ptr::addr_of!(color));
+}
+
+pub unsafe fn DrawText(text: *const i8, x: i32, y: i32, size: i32, color: Color) {
+    DrawText_(text, x, y, size, std::ptr::addr_of!(color));
+}
+
+pub unsafe fn DrawRectangleRec(rec: Rectangle, color: Color) {
+    DrawRectangleRec_(std::ptr::addr_of!(rec), std::ptr::addr_of!(color));
+}
