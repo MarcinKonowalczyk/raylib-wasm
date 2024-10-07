1ea6a7b53a01ac6b11f88c8b0dc20cbc002ff055
Mark Tyrkba <awfulmarco@gmail.com>
2024-10-07 18:36:43 +0300

    INIT

---
+++ README.md
@@ -0,0 +1,2 @@
+# raylib-light
+These are just raw bindings of raylib 5.0 so you can use raylib pretty much as you will in C, though I am in a process of implementing a thing, that will let you run the same raylib game in browser, that you can run natively, with almost NO changes in the source code.
---
+++ raylib.rs
@@ -0,0 +1,17 @@
+mod macros;
+#[allow(unused)]
+pub use macros::*;
+mod colors;
+#[allow(unused)]
+pub use colors::*;
+mod enums;
+#[allow(unused)]
+pub use enums::*;
+#[allow(unused, dead_code, non_camel_case_types, non_snake_case)]
+mod structs;
+#[allow(unused)]
+pub use structs::*;
+#[allow(unused, dead_code, non_camel_case_types, non_snake_case)]
+mod fns;
+#[allow(unused)]
+pub use fns::*;
