0ab92d286aa04fd930e2518fea127bb5e95861ab
Mark Tyrkba <awfulmarco@gmail.com>
2025-02-13 18:22:24 +0300

    Fix up the nul-termination bug

--- macros.rs
+++ macros.rs
@@ -1,4 +1,4 @@
 #[macro_export]
 macro_rules! cstr {
-    ($($arg: tt)*) => { std::fmt::format(format_args!($($arg)*)).as_ptr() as *const i8 }
+    ($($arg: tt)*) => { std::fmt::format("{}\0", format_args!($($arg)*)).as_ptr() as *const i8 }
 }
