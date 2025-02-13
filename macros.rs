201d5772ce3e111976c35b693e00cb4c10f795b0
Mark Tyrkba <awfulmarco@gmail.com>
2025-02-13 18:48:10 +0300

    Get back the old `cstr!` macro

--- macros.rs
+++ macros.rs
@@ -1,4 +1,4 @@
 #[macro_export]
 macro_rules! cstr {
-    ($($arg: tt)*) => { std::fmt::format("{}\0", format_args!($($arg)*)).as_ptr() as *const i8 }
+    ($str: expr) => { format!("{}\0", $str).as_ptr() as *const i8 }
 }
