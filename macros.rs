533f342e2d5bdc0a8bad6b50b0e456684f7059e4
Mark Tyrkba <awfulmarco@gmail.com>
2025-02-13 16:39:29 +0300

    Update `cstr` macro

--- macros.rs
+++ macros.rs
@@ -1,4 +1,4 @@
 #[macro_export]
 macro_rules! cstr {
-    ($str: expr) => { format!("{}\0", $str).as_ptr() as *const i8 }
+    ($($arg: tt)*) => { std::fmt::format(format_args!($($arg)*)).as_ptr() as *const i8 }
 }
