c97f538e629baade411aa9065c6b4b7d76e1593e
Marcin Konowalczyk <marcin.konow@lczyk.xyz>
2025-02-13 19:55:37 +0400

    cstr! update

--- macros.rs
+++ macros.rs
@@ -1,4 +1,6 @@
 #[macro_export]
 macro_rules! cstr {
-    ($str: expr) => { format!("{}\0", $str).as_ptr() as *const i8 }
+    ($($arg: tt)*) => {
+        std::ffi::CString::new(format!($($arg)*)).unwrap()
+    }
 }
